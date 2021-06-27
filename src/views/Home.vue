1d6d0057b57b9f5205fc61ff82519717b159732f
Nisse Bengtsson <benzzon@gmail.com>
2021-06-27 22:34:51 +0200

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="home">
     <h1>Homepage</h1>
+    <p class="blue lighten-3 black--text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore explicabo temporibus deleniti, perspiciatis officiis autem id, modi nostrum sit officia nihil impedit saepe repudiandae unde delectus enim doloribus illum voluptatem.</p>
 </div>
 </template>
 
